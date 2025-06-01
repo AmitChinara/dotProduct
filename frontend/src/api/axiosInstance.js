f7f8c615fe326917df6da40f8dd434f03584e920
Amit Chinara <amitchinara@gmail.com>
2025-06-01 23:30:16 +0530

    Final Commit v1.0

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: 'https://vercel.com/amitchinaras-projects/dot-product/api/',
 });
 
 // Use 'authToken' to match your login/localStorage usage
